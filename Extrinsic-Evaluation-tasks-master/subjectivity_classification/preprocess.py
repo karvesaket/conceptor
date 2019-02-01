642a87ec8e9e3846b51d010d08e49a58c541ce67
karvesaket <46728782+karvesaket@users.noreply.github.com>
2019-01-31 19:53:01 -0500

    Update preprocess.py

--- Extrinsic-Evaluation-tasks-master/subjectivity_classification/preprocess.py
+++ Extrinsic-Evaluation-tasks-master/subjectivity_classification/preprocess.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import numpy as np
 import gzip
 import os
+from gensim.models.keyedvectors import KeyedVectors
 
 import sys
 if (sys.version_info > (3, 0)):
@@ -104,35 +105,42 @@ wordEmbeddings = []
 
 
 # :: Load the pre-trained embeddings file ::
-fEmbeddings = open(epath)
+#fEmbeddings = open(epath)
+embd_type = input("Enter embedding type")
+if embd_type == "glove":
+    resourceFile = '/content/'
+    curr_embd = KeyedVectors.load_word2vec_format(resourceFile + 'gensim_glove.840B.300d.txt.bin', binary=True)
+    print(curr_embd.vectors.shape[1])
+elif embd_type == "word2vec":
+    resourceFile = '/content/'
+    curr_embd = KeyedVectors.load_word2vec_format(resourceFile + 'GoogleNews-vectors-negative300.bin', binary=True)                       
+print('The embedding has been loaded from gensim!')
 
 print("Load pre-trained embeddings file")
-for line in fEmbeddings:
-    split = line.strip().split(" ")
-    if len(split) == 2:
-        continue
-    word = split[0]
+embd_dim = curr_embd.vectors.shape[1]
+    for word in words:
+        if len(word2Idx) == 0: #Add padding+unknown
+            word2Idx["PADDING_TOKEN"] = len(word2Idx)
+            vector = np.zeros(embd_dim) #Zero vector vor 'PADDING' word
+            wordEmbeddings.append(vector)
 
-    if len(word2Idx) == 0: #Add padding+unknown
-        word2Idx["PADDING_TOKEN"] = len(word2Idx)
-        vector = np.zeros(len(split)-1) #Zero vector vor 'PADDING' word
-        wordEmbeddings.append(vector)
-
-        word2Idx["UNKNOWN_TOKEN"] = len(word2Idx)
-        vector = np.random.uniform(-0.25, 0.25, len(split)-1)
-        wordEmbeddings.append(vector)
-
-    if word.lower() in words:
-        vector = np.array([float(num) for num in split[1:]])
-        wordEmbeddings.append(vector)
-        word2Idx[word] = len(word2Idx)
-
+            word2Idx["UNKNOWN_TOKEN"] = len(word2Idx)
+            vector = curr_embd['unk']
+            wordEmbeddings.append(vector)
+        if word in curr_embd:
+            vector = curr_embd[word]
+            wordEmbeddings.append(vector)
+            word2Idx[word] = len(word2Idx)
 
 wordEmbeddings = np.array(wordEmbeddings)
 
 print("Embeddings shape: ", wordEmbeddings.shape)
 print("Len words: ", len(words))
-
+con = input("Conceptor? ")
+    if con == "y":
+        wordEmbeddings_new = post_process_cn_matrix(wordEmbeddings.T)
+    else:
+        wordEmbeddings_new = wordEmbeddings
 
 
 # :: Create matrices ::
@@ -142,7 +150,7 @@ test_matrix = createMatrices(testDataset[0], word2Idx)
 
 
 data = {
-    'wordEmbeddings': wordEmbeddings, 'word2Idx': word2Idx,
+    'wordEmbeddings': wordEmbeddings_new, 'word2Idx': word2Idx,
     'train': {'sentences': train_matrix, 'labels': trainDataset[1]},
     'dev':   {'sentences': dev_matrix, 'labels': devDataset[1]},
     'test':  {'sentences': test_matrix, 'labels': testDataset[1]}
